013ff3df7723b363aa174daadd6f50cccfe71ab3
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-01-25 13:21:11 +0200

    feat: reworked hero page

--- src/components/shared/ball.tsx
+++
@@ -1,9 +0,0 @@
-export interface Props {
-  type: '1' | '2'
-  className?: string
-}
-export const Ball = ({ type, className }: Props) => {
-  const ballClass = type === '1' ? 'g-ball-1' : 'g-ball-2'
-
-  return <div className={`g-ball-general ${ballClass} ${className}`}></div>
-}

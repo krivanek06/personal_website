5323026fb58c9ff4510f0dfc9a52cd44a1c21a55
krivanek06 <krivanek1234@gmail.com>
2023-01-29 19:49:33 +0200

    chore: animation adjust

--- src/components/functions/text-modification.ts
+++ src/components/functions/text-modification.ts
@@ -4,28 +4,42 @@ export const textModification = (
   inputText: string,
   setInputText: (s: string) => void,
   longRuntime = false,
+  stopRecursion = false,
+  nonsenseText = '',
 ) => {
   let iteration = 0
 
-  const interval = setInterval(() => {
-    const newTex = inputText
-      .split('')
-      .map((letter, index) => {
-        // if (index < iteration) {
-        //   return inputText[index]
-        // }
-        return LETTERS[Math.floor(Math.random() * 26)]
-      })
-      .join('')
+  const interval = setInterval(
+    () => {
+      const newTex = inputText
+        .split('')
+        .map((_, index) => {
+          // return unchanged first characters
+          if (stopRecursion && index < iteration) {
+            return inputText[index]
+          }
 
-    // change input
-    setInputText(newTex)
+          // recursion happened
+          return nonsenseText[index] ?? LETTERS[Math.floor(Math.random() * 26)]
+        })
+        .join('')
 
-    if (iteration >= inputText.length) {
-      console.log('clearing')
-      clearInterval(interval)
-      setInputText(inputText)
-    }
-    iteration += longRuntime ? 1 / 3 : 1
-  }, 50)
+      // change input
+      setInputText(newTex)
+
+      if (iteration >= inputText.length) {
+        clearInterval(interval)
+
+        // recursion
+        if (!stopRecursion) {
+          console.log('stopRecursion')
+          textModification(inputText, setInputText, longRuntime, true, newTex)
+        }
+      }
+
+      // no recursion - go fast
+      iteration += !stopRecursion && longRuntime ? 1 / 3 : 1
+    },
+    stopRecursion ? 90 : 50,
+  )
 }

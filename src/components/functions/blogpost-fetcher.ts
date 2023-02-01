0c4e28fadd00909328aa9be0ce01b181b8e889d1
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-02-01 23:38:53 +0200

    feat: showing blogposts

---
+++ src/components/functions/blogpost-fetcher.ts
@@ -0,0 +1,68 @@
+import { DEV_TO_BLOGS } from '../../environments/environment'
+import type { BlogPost, BlogPostLocalHost, DevToBlog } from '../../models'
+import { isDateToday } from './date.util'
+
+const LOCAL_STORAGE_KEY = 'dev_to_blogs'
+
+export const getBlogPosts = async (): Promise<BlogPost[]> => {
+  // check in local
+  const localBlogs = getBlogPostFromLocalHost()
+  if (localBlogs) {
+    return localBlogs
+  }
+
+  // fetch remote
+  const remoteBlogs = await fetchBlogPosts()
+
+  // format
+  const remoteBlogsFormatted = remoteBlogs.map((d) => formatBlogPosts(d))
+
+  // save to local
+  saveBlogPostToLocalHost(remoteBlogsFormatted)
+
+  return remoteBlogsFormatted
+}
+
+const fetchBlogPosts = async (): Promise<DevToBlog[]> => {
+  const data = (await (await fetch(DEV_TO_BLOGS)).json()) as DevToBlog[]
+  return data
+}
+
+const formatBlogPosts = (devToBlog: DevToBlog): BlogPost => {
+  return {
+    title: devToBlog.title,
+    description: devToBlog.description,
+    imageUrl: devToBlog?.cover_image ?? devToBlog.social_image,
+    postUrl: devToBlog.url,
+    publishedDate: devToBlog.readable_publish_date,
+    userFullName: devToBlog.user.name,
+    userProfileImageUrl: devToBlog.user.profile_image,
+  }
+}
+
+const saveBlogPostToLocalHost = (posts: BlogPost[]): void => {
+  const date = new Date()
+  const blogLocal: BlogPostLocalHost = {
+    date,
+    posts,
+  }
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(blogLocal))
+}
+
+const getBlogPostFromLocalHost = (): BlogPost[] | undefined => {
+  const localData = localStorage.getItem(LOCAL_STORAGE_KEY)
+
+  // not in local storage
+  if (!localData) {
+    return undefined
+  }
+
+  const localDataParsed = JSON.parse(localData) as BlogPostLocalHost
+
+  // old date
+  if (!isDateToday(localDataParsed.date)) {
+    return undefined
+  }
+
+  return localDataParsed.posts
+}
--- src/components/modules/about-me.tsx
+++ src/components/modules/about-me.tsx
@@ -1,11 +1,14 @@
-import type { ContentProjectionProps } from '../../models'
+import { useEffect, useState } from 'react'
+import type { BlogPost, ContentProjectionProps } from '../../models'
+import { getBlogPosts } from '../functions'
+import { CardBlogPost } from '../shared/card-blogpost'
 import { CardPresentation2Content } from '../shared/card-presentation-2'
 
 export const AboutMePage = () => {
   return (
     <div className="relative">
       <AboutMeBackground>
-        <div className="g-layout-1 h-[135vh]">
+        <div className="g-layout-wide h-[135vh]">
           <AboutMeContent></AboutMeContent>
         </div>
       </AboutMeBackground>
@@ -37,42 +40,91 @@ const AboutMeContent = () => {
         <span className=" g-text-shadow-primary">Me</span>
       </h2>
 
-      <div className="grid lg:grid-cols-2 min-h-[90vh] ">
+      <div className="grid lg:grid-cols-2 xl:grid-cols-3 min-h-[90vh] ">
         <div className="relative hidden lg:block">
           {/* picture */}
           <img
             src="/images/myself_only_body_3.png"
             alt="author image"
-            className="h-full object-cover absolute  -z-10"
+            className="h-full object-cover absolute -z-10 top-[-5%]"
           />
           {/* card */}
           <CardPresentation2Content
             bgClasses="bg-g-primary-transparent-medium hover:bg-g-primary-transparent-dark"
             classes="w-[420px] bottom-0 absolute right-[-20%] opacity-70 hover:opacity-90 "
           >
-            <p className="w-11/12 m-auto text-g-gray flex flex-col gap-4">
-              <span>
-                As a professional web developer, I have a passion for creating high-quality, visually
-                stunning, and easy-to-use web applications.
-              </span>
-              <span>
-                With several years of experience in both front-end and back-end development, I am well-versed
-                in a variety of technologies, including Angular, NestJS, GraphQL, and REST API's. I specialize
-                in delivering custom web solutions to clients with unique needs, and work closely with them to
-                understand their requirements and design a solution that fits their specific needs.
-              </span>
-              <span>
-                My goal is to help businesses stay on the cutting edge of technology by creating modern,
-                efficient, and high-performing web applications, whether it's a new project or software
-                modernization.
-              </span>
-            </p>
+            <AboutMeText />
           </CardPresentation2Content>
         </div>
 
         {/* blog posts */}
-        <div>blogposts</div>
+        <div className="grid place-content-center xl:col-span-2">
+          <AboutMeBlogPosts />
+        </div>
       </div>
+    </>
+  )
+}
+
+const AboutMeText = () => {
+  return (
+    <p className="w-11/12 m-auto text-g-gray flex flex-col gap-4">
+      <span>
+        As a professional web developer, I have a passion for creating high-quality, visually stunning, and
+        easy-to-use web applications.
+      </span>
+      <span>
+        With several years of experience in both front-end and back-end development, I am well-versed in a
+        variety of technologies, including Angular, NestJS, GraphQL, and REST API's. I specialize in
+        delivering custom web solutions to clients with unique needs, and work closely with them to understand
+        their requirements and design a solution that fits their specific needs.
+      </span>
+      <span>
+        My goal is to help businesses stay on the cutting edge of technology by creating modern, efficient,
+        and high-performing web applications, whether it's a new project or software modernization.
+      </span>
+    </p>
+  )
+}
+
+const AboutMeBlogPosts = () => {
+  const [data, setData] = useState<BlogPost[]>([])
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await getBlogPosts()
+
+      console.log(data)
+      setData(data)
+    }
+
+    dataFetch()
+  }, [])
+
+  const moveLeft = 'ml-[-100px] mr-[100px]'
+  const moveRight = 'ml-[100px] mr-[-100px]'
+
+  return (
+    <>
+      {data.map((d, index) => (
+        <div
+          key={d.title}
+          className={`max-h-[230px] -mb-10 bg-black rou rounded-lg hover:z-10 ${
+            index % 2 === 0 ? moveLeft : moveRight
+          }`}
+        >
+          <CardBlogPost
+            title={d.title}
+            description={d.description}
+            imageUrl={d.imageUrl}
+            postUrl={d.postUrl}
+            publishedDate={d.publishedDate}
+            userFullName={d.userFullName}
+            userProfileImageUrl={d.userProfileImageUrl}
+          />
+        </div>
+      ))}
     </>
   )
 }

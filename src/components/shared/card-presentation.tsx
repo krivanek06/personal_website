0aeea8edc4226e7c623b7ab42f141904f4440ae6
krivanek06 <krivanek1234@gmail.com>
2023-05-07 15:41:16 +0300

    chore: changed comment

--- src/components/shared/card-presentation.tsx
+++ src/components/shared/card-presentation.tsx
@@ -19,7 +19,7 @@ export const CardPresentation = ({ title, description, img, additionalClasses }:
         className="object-cover absolute opacity-70 group-hover:opacity-90 duration-700 h-full w-full "
       />
 
-      {/* dark explanation */}
+      {/* card content */}
       <div className="text-center p-4 absolute flex items-center justify-center w-full bottom-[30%] group-hover:bottom-2 duration-700  min-h-[110px] bg-g-overlay-dark hover:bg-g-overlay-medium flex-col gap-0 group-hover:gap-4  ">
         <h3 className="text-g-primary g-heading-3 sm:animate-pulse group-hover:animate-none">{title}</h3>
         <p className="g-fade scale-0 h-0 group-hover:h-auto group-hover:scale-100 pb-4 ">{description}</p>
